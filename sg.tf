c41529c7e85c4a6e30dd3ff89b2393e56702434e
Kwasi <shev030391@gmail.com>
2024-07-28 17:47:38 -0400

    initial commit

---
+++ sg.tf
@@ -0,0 +1,56 @@
+resource "aws_security_group" "sg-demo" {
+  name        = "alb-sg"
+  description = "Allow  http"
+  vpc_id      = aws_vpc.vp1.id
+
+
+
+  ingress {
+    description = "allow http"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    env = "Dev"
+  }
+  depends_on = [aws_vpc.vp1]
+}
+
+
+
+
+resource "aws_security_group" "sg-demo1" {
+  name        = "web-sg"
+  description = "Allow  http"
+  vpc_id      = aws_vpc.vp1.id
+
+
+
+  ingress {
+    description     = "allow http"
+    from_port       = 80
+    to_port         = 80
+    protocol        = "tcp"
+    security_groups = [aws_security_group.sg-demo.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    env = "Dev"
+  }
+  depends_on = [aws_vpc.vp1, aws_security_group.sg-demo]
+}
